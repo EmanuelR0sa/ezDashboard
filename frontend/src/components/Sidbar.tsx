6a25ed2ee0a525125df0aa8caad06a0ea9fc146d
EmanuelR0sa <emanuelnzinga@gmail.com>
2023-12-08 10:29:50 +0000

    added react icons

--- frontend/src/components/Sidbar.tsx
+++ frontend/src/components/Sidbar.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { FiMonitor } from "react-icons/fi";
 export default function Sidbar() {
   return (
     <div className="w-56 h-screen bg-slate-200">
       <div className="flex justify-center">
         <div className=" flex flex-col text-base mt-28">
-          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/dashboard" className="flex items-center"> <div className="text-xl pr-3"><FiMonitor/></div>  Dashboard</Link>
           <Link to="/projects">Projects</Link>
           <Link to="/income">Income</Link>
           <Link to="/expenses">Expenses</Link>
